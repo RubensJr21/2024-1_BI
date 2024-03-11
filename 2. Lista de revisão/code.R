266ee78221983ce7c9ad7dd1f920cd70da5e593a
Rubens Do Nascimento Corrêa Júnior <20191BSI0379@cefetes.br>
2024-03-11 11:10:36 -0300

    Começo da resolução da lista de revisão 2

---
+++ 2. Lista de revisão/code.R
@@ -0,0 +1,61 @@
+# 1. Crie um vetor chamado idades com as idades de 5 pessoas: 25, 30, 22, 35, 28. Em seguida, calcule a média das idades (mean).
+
+idades <- c(25, 30, 22, 35, 28)
+mean(idades)
+
+# 2. Use um loop for para imprimir os números de 1 a 5.
+for (num in seq(1:5)) {
+  print(num)
+}
+
+# 3. Crie uma função chamada classificaIdade que recebe uma idade como parâmetro e retorna "Menor de idade" se a idade for menor que 18, e "Maior de idade" caso contrário.
+
+classificaIdade <- function(idade) {
+  if(idade < 18){
+    return ("Menor de idade")
+  }else{
+    return ("Maior de idade")
+  }
+}
+
+classificaIdade(17)
+classificaIdade(23)
+
+# 4. Crie uma função chamada somaQuadrados que recebe dois números como parâmetros e retorna a soma de seus quadrados (X ^ 2).
+
+somaQuadrados <- function(num1, num2){
+  return ((num1 ^ 2) + (num2 ^ 2))
+}
+
+somaQuadrados(2,5)
+somaQuadrados(7,13)
+
+# 5. Utilize a função sapply para calcular o dobro de cada elemento no vetor numeros (1, 2, 3, 4, 5).
+
+number.dobro <- function(numero){
+  return (2 * numero)
+}
+
+numeros <- c(1, 2, 3, 4, 5)
+sapply(numeros, number.dobro)
+
+# 6. Crie um script que gera um gráfico de dispersão (plot) para visualizar a relação entre duas variáveis fictícias, como idade e pontuação em um teste. Use cores diferentes para representar se a pessoa é menor ou maior de idade. Crie uma função chamada plotIdadePontuacao que aceita vetores de idades e pontuações como parâmetros. Seu script deve criar um gráfico de dispersão onde cada ponto representa uma pessoa com sua idade e pontuação, e a cor do ponto indica se a pessoa é menor ou maior de idade. A função legend adiciona uma legenda ao gráfico indicando as cores associadas a cada grupo.
+
+# idade e pontuação de um teste
+# cor diferente para diferenciar os maiores de idade dos menores de idade
+# cirar função plotIdadePontuacao que aceita vetores de idades e pontuações como parâmetros.
+
+install.packages("ggplot2")
+library("ggplot2")
+
+pessoas.idades <- c(15,14,16)
+pessoas.pontuacao <- c(5,9,5)
+
+plotIdadePontuacao <- function(idades,pontuacoes){
+  pessoas <- list(idades, pontuacoes)
+  ggplot(pessoas, aes(idades)) + geom_dotplot()
+}
+
+plotIdadePontuacao(pessoas.idades, pessoas.pontuacao)
+
+??ggplot
